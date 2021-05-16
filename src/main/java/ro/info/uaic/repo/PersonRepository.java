48ff797ade33eb792c9872bb29f68ef72640d1ba
Dragos101 <57534351+Dragos101@users.noreply.github.com>
2021-05-16 22:50:59 +0300

    final update v3

--- src/main/java/ro/info/uaic/repo/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.info.uaic.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import ro.info.uaic.entity.PersonEnt;
-
-public interface PersonRepository extends CrudRepository<PersonEnt, String> {
-
-}
