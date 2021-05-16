cd0f1b2be3cfcdf11832705b2580add402ad7f23
Dragos101 <57534351+Dragos101@users.noreply.github.com>
2021-05-16 22:20:42 +0300

    +update compulsory

--- src/main/java/ro/info/uaic/repo/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.info.uaic.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import ro.info.uaic.entity.PlayerEnt;
-
-public interface PlayerRepository extends CrudRepository<PlayerEnt, String> {
-
-}
